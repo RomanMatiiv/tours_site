09dfd9a88a7a5196f906cefe1821af939176e4bd
matiiv <matiiv.r.a@gmail.com>
2022-01-30 01:25:58 +0300

    change ALLOWED_HOSTS

--- tours_site/settings.py
+++ tours_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&dax5q3q5(df+44ad7t*f%0a+hpz$kx2(l3=+j1@lwndu^uo@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
